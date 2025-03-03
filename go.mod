b5a2599991981053b9b785752c96170bb70af474
Matteo Mortari <matteo.mortari@gmail.com>
2025-03-03 15:04:05 +0100

    core: update logic for InitContainer use-case (#18)
    * core: update logic for InitContainer use-case
    
    Signed-off-by: tarilabs <matteo.mortari@gmail.com>
    
    * revert to Go 1.22 to availability of go-toolset
    
    Signed-off-by: tarilabs <matteo.mortari@gmail.com>
    
    ---------
    
    Signed-off-by: tarilabs <matteo.mortari@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module example/hello
+module link-model-and-wait
 
-go 1.22.4
+go 1.22
--- link-model-and-wait.go
+++ link-model-and-wait.go
@@ -11,12 +11,28 @@ import (
 
 func main() {
 	fmt.Println("Hello, world! And thanks Jason for the idea, Daniele and Roland")
-	fmt.Println("Invoked with and will check later is consistent with KServe deployment", strings.Join(os.Args, " ")) // TODO will need to check the args here.
+	checkIfEarlyReturn()
+	fmt.Println("Creating symbolic link per ModelCar logic...")
 	doTheThing()
+	fmt.Println("Will now wait forever to serve as sidecar per ModelCar logic. (waiting forever)")
 	exitSignal := make(chan os.Signal, 1)
 	signal.Notify(exitSignal, syscall.SIGINT, syscall.SIGTERM)
 	<-exitSignal
 	fmt.Println("Main terminated.")
+}
+
+func checkIfEarlyReturn() {
+	fmt.Println("Invoked as:", strings.Join(os.Args, " "))
+	shouldEarlyReturn := true
+	for _, arg := range os.Args {
+		if strings.Contains(arg, "sleep") {
+			shouldEarlyReturn = false
+		}
+	}
+	if shouldEarlyReturn {
+		fmt.Println("Did not find any use of `sleep` in arguments, assuming InitContainer, terminating early with zero return code.")
+		os.Exit(0)
+	}
 }
 
 func doTheThing() {
@@ -24,7 +40,7 @@ func doTheThing() {
 	fromLink := filepath.Join("/proc", fmt.Sprintf("%d", pid), "root/models")
 	toLink := "/mnt/models"
 
-	if _, err := os.Lstat(toLink); err == nil {
+	if _, err := os.Lstat(toLink); err == nil { // see also https://github.com/kserve/kserve/pull/4274 for force recreation of symlink
 		fmt.Printf("Link path %s already exists. Removing it.\n", toLink)
 		err := os.Remove(toLink)
 		if err != nil {
